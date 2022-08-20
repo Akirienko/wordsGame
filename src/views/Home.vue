005767c7ed1ce22e93859142c53af9034b987010
@Akirienko <lakirienkol@gmail.com>
2022-08-20 23:57:09 +0300

    add new word to api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,14 +6,40 @@
         You are visited my app for learning words
       </p>
     </div>
-    <div class="">
-      <button @click="random" class="">CLICK TO START</button>
-      <div
-        @click="transl"
-        class="w-52 h-16 bg-red-500 flex items-center justify-center"
+    <form
+      action=""
+      class="flex flex-col items-center"
+      @submit.prevent="addNewWord"
+    >
+      <h3 class="text-base mb-2">Add new word!</h3>
+      <input
+        class="w-40 mb-4 rounded-lg h-8 boxShadow p-4"
+        type="text"
+        v-model="newWord"
+      />
+      <input
+        class="w-40 rounded-lg h-8 boxShadow p-4"
+        type="text"
+        v-model="newWordTranslate"
+      />
+      <button
+        :disabled="!newWord && !newWordTranslate"
+        class="button w-40 h-8 mt-4 rounded-lg bg-green-400 border-solid border-2 border-black"
       >
-        <p>{{ randomWord }}</p>
-        <p>{{ wordTranslate }}</p>
+        Add
+      </button>
+    </form>
+    <div class="flex items-center justify-center flex-col">
+      <button @click="random" class="mb-24 mt-14">CLICK TO START</button>
+      <div
+        @click="translating"
+        class="w-52 h-16 flex items-center justify-center wordBoxColor rounded-xl"
+      >
+        <div class="overflow-hidden w-full h-full">
+          <p class="capitalize text-lg absolute center flip-animate">
+            <span :data-hover="wordTranslate">{{ randomWord }} </span>
+          </p>
+        </div>
       </div>
     </div>
   </div>
@@ -21,28 +47,54 @@
 
 <script setup>
 import { ref, onMounted } from "vue";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, onSnapshot, addDoc } from "firebase/firestore";
 import { db } from "@/firebase";
 
 const words = ref([]);
+
 const randomWords = ref([]);
 const randomWord = ref("");
 const wordTranslate = ref("");
 
-//// query api
-onMounted(async () => {
-  const getWords = await getDocs(collection(db, "words"));
-  let fbWords = [];
-  getWords.forEach((doc) => {
-    // words.value = doc.data();
-    const word = {
-      id: doc.id,
-      word: doc.data().word,
-      translate: doc.data().translate,
-    };
-    fbWords.push(word);
+const newWord = ref("");
+const newWordTranslate = ref("");
+
+const addNewWord = () => {
+  addDoc(collection(db, "words"), {
+    word: newWord.value,
+    translating: newWordTranslate.value,
   });
-  words.value = fbWords;
+
+  newWord.value = "";
+  newWordTranslate.value = "";
+};
+
+//// query api
+onMounted(() => {
+  // const getWords = getDocs(collection(db, "words"));
+  // getWords.forEach((doc) => {
+  //   // words.value = doc.data();
+  //   const word = {
+  //     id: doc.id,
+  //     word: doc.data().word,
+  //     translate: doc.data().translate,
+  //   };
+  //   fbWords.push(word);
+  // });
+  onSnapshot(collection(db, "words"), (querySnapshot) => {
+    // const cities = [];
+    let fbWords = [];
+    querySnapshot.forEach((doc) => {
+      const word = {
+        id: doc.id,
+        word: doc.data().word,
+        translate: doc.data().translate,
+      };
+      fbWords.push(word);
+    });
+    // console.log("Current cities in CA: ", cities.join(", "));
+    words.value = fbWords;
+  });
 });
 
 //// add random word
@@ -56,7 +108,7 @@ const random = () => {
   // console.log(randomWords.value);
 };
 
-const transl = () => {
+const translating = () => {
   console.log(randomWords.value);
   wordTranslate.value = randomWords.value.translate;
 };
@@ -94,3 +146,42 @@ const transl = () => {
 //     .catch((err) => console.error(err));
 // };
 </script>
+
+<style scoped>
+.flip-animate:hover,
+.flip-animate:focus {
+  color: #222;
+}
+
+.flip-animate {
+  perspective: 1000px;
+}
+.flip-animate span {
+  position: relative;
+  display: inline-block;
+  padding: 0;
+  transition: transform 0.3s;
+  transform-origin: 50% 0;
+  transform-style: preserve-3d;
+}
+.flip-animate span:before {
+  position: absolute;
+  top: 100%;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  content: attr(data-hover);
+  transition: color 0.3s;
+  transform: rotateX(-90deg);
+  transform-origin: 50% 0;
+  text-align: center;
+}
+.flip-animate:hover span,
+.flip-animate:focus span {
+  transform: rotateX(90deg) translateY(-22px);
+}
+.flip-animate:hover span:before,
+.flip-animate:focus span:before {
+  color: #d24936;
+}
+</style>
